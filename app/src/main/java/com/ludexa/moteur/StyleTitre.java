4bd886eb2ae49823631b17569fa936e8350de793
Yop2d-dev <candy.thoury@gmail.com>
2026-09-09 22:54:25 +0200

    Add letter spacing and line multiplier to StyleTitre
    Added letter spacing and line multiplier properties.

--- app/src/main/java/com/ludexa/moteur/StyleTitre.java
+++ app/src/main/java/com/ludexa/moteur/StyleTitre.java
@@ -1,25 +1,24 @@
-// haut 1
 package com.ludexa.moteur;
 
 import android.graphics.Color;
 
 public class StyleTitre {
     public String nom;
-    public String modeRemplissage = "FILL_AND_STROKE"; // FILL, STROKE, FILL_AND_STROKE
+    public String modeRemplissage = "FILL_AND_STROKE";
     public float epaisseurContour = 4f;
     public int couleurContour = Color.BLACK;
-    
     public boolean utiliserDegrade = false;
     public int couleurDegrade1 = Color.WHITE;
     public int couleurDegrade2 = Color.GRAY;
-    
     public boolean ombreActive = false;
     public float ombreRayon = 5f;
     public float ombreDx = 5f;
     public float ombreDy = 5f;
     public int ombreCouleur = Color.BLACK;
     
-    public String cheminPolice = null;
+    // --- NOUVEAUTÉS : ESPACEMENT ---
+    public float espacementLettres = 0.05f; // En EM (0.05 = léger, 0.1 = espacé)
+    public float multiplicateurLignes = 1.2f;
 
     public StyleTitre() {}
 
@@ -27,4 +26,3 @@ public class StyleTitre {
         this.nom = nom;
     }
 }
-// bas 1
