b2226e7fc4c1d32f2eec0c3493a25d0b0aae7062
Yop2d-dev <candy.thoury@gmail.com>
2026-09-11 17:21:33 +0200

    Update EcranDemarrage.java

--- app/src/main/java/com/ludexa/moteur/EcranDemarrage.java
+++ app/src/main/java/com/ludexa/moteur/EcranDemarrage.java
@@ -1419,7 +1419,7 @@ public class EcranDemarrage extends Activity {
             .show();
     }
 
-    private void telechargerMiseAJour(String url) {
+        private void telechargerMiseAJour(String url) {
         try {
             android.app.DownloadManager.Request requete = new android.app.DownloadManager.Request(Uri.parse(url));
             requete.setTitle(Traducteur.get("maj_titre_dl"));
@@ -1434,6 +1434,21 @@ public class EcranDemarrage extends Activity {
         } catch (Exception e) {
             Toast.makeText(this, Traducteur.get("maj_echec_natif"), Toast.LENGTH_SHORT).show();
             ouvrirLien(url);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
         }
     }
 }
