eb28c86ad8f95be72b3a6838be96ff6b7eb1b66d
Yop2d-dev <candy.thoury@gmail.com>
2026-09-14 17:44:45 +0200

    Update DialogueGestionAssets.java

--- app/src/main/java/com/ludexa/moteur/DialogueGestionAssets.java
+++ app/src/main/java/com/ludexa/moteur/DialogueGestionAssets.java
@@ -67,6 +67,21 @@ public class DialogueGestionAssets extends Dialog {
         }
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && getWindow() != null) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
+        }
+    }
+
     private int dp(int valeur) {
         return (int) (valeur * ctx.getResources().getDisplayMetrics().density);
     }
