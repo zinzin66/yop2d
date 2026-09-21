557f8c477207609b839df4f4a739d9e22db53a8e
Yop2d-dev <candy.thoury@gmail.com>
2026-09-21 16:40:02 +0200

    Add new action cases for si_au_sol and si_en_mouvement

--- app/src/main/java/com/ludexa/moteur/ActionsMoteur.java
+++ app/src/main/java/com/ludexa/moteur/ActionsMoteur.java
@@ -48,6 +48,8 @@ public class ActionsMoteur {
             case "modifier_deplacable": ActionsMouvements.modifierDeplacable(n); return null;
             case "sautiller": ActionsMouvements.sautiller(n); return null;
             case "orienter_vers": ActionsMouvements.orienterVers(n); return null;
+            case "si_au_sol": return ActionsMouvements.siAuSol(n);
+            case "si_en_mouvement": return ActionsMouvements.siEnMouvement(n);
             case "journaliser": ActionsOutils.journaliser(n); return null;
             case "attendre": return ActionsTemps.attendre(n);
             case "repeter": return ActionsTemps.repeter(n);
