8886c0ddb97abc461c03a8b29ef676053a0f1f42
Yop2d-dev <candy.thoury@gmail.com>
2026-09-22 11:41:26 +0200

    Rename NoeudEventSortieZone to NoeudEventEntreeZone

--- app/src/main/java/com/ludexa/moteur/NoeudEventEntreeZone.java
+++ app/src/main/java/com/ludexa/moteur/NoeudEventEntreeZone.java
@@ -4,27 +4,33 @@ package com.ludexa.moteur;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NoeudEventSortieZone extends NoeudBase {
+public class NoeudEventEntreeZone extends NoeudBase {
 
-    private transient ObjetBase cibleA;
-    // SUPPRIMÉ ET RENOMMÉ : private String nomCibleObjetA; (Utilise désormais this.nomCibleObjet hérité de NoeudBase)
-    
-    private transient ObjetBase cibleB;
-    // SUPPRIMÉ : private String nomCibleObjetB; (Utilise désormais this.nomCibleObjetB hérité de NoeudBase)
-    
-    private transient boolean etaitEnCollision = false;
+    private ObjetBase cible;
 
-    public NoeudEventSortieZone() {
-        super(genererId(), "noeud_sortie_de_zone", "Événements");
-        this.ajouterPort(new Port("Suivant", Port.TYPE_EXECUTION_SORTIE));
+    public NoeudEventEntreeZone() {
+        super(genererId(), "noeud_entree_de_zone", "Evenement");
+        this.ajouterPort(new Port("Executer", Port.TYPE_EXECUTION_SORTIE));
     }
-    
-    public boolean isEtaitEnCollision() { return etaitEnCollision; }
-    public void setEtaitEnCollision(boolean val) { this.etaitEnCollision = val; }
 
     @Override
     public void executer() {
-        propagerExecution("Suivant");
+        propagerExecution("Executer");
+    }
+
+    @Override
+    public boolean requiertCibleObjet() { return true; }
+
+    @Override
+    public void setCibleObjet(ObjetBase objet) { 
+        this.cible = objet; 
+        this.nomCibleObjet = (objet != null) ? objet.nom : null;
+    }
+
+    @Override
+    public ObjetBase getCibleObjet() { 
+        if ("__OBJET_IMPLIQUE__".equals(nomCibleObjet)) return MoteurLogique.dernierObjetImplique;
+        return this.cible; 
     }
 
     @Override
@@ -34,70 +40,9 @@ public class NoeudEventSortieZone extends NoeudBase {
     public String getValeurParametre(String nom) { return ""; }
 
     @Override
-    public void setValeurParametre(String nom, String valeur) { }
+    public void setValeurParametre(String nom, String valeur) {}
 
     @Override
-    public boolean requiertCibleObjet() { return true; }
-    
-    @Override
-    public void setCibleObjet(ObjetBase objet) { 
-        this.cibleA = objet;
-        this.nomCibleObjet = (objet != null) ? objet.nom : null;
-    }
-    
-    @Override
-    public ObjetBase getCibleObjet() {
-        if ("__OBJET_IMPLIQUE__".equals(nomCibleObjet)) return MoteurLogique.dernierObjetImplique;
-
-        if (cibleA == null && nomCibleObjet != null && contexteApplication != null) {
-            try {
-                if (contexteApplication instanceof InterfaceEditeur) {
-                    Scene s = ((InterfaceEditeur) contexteApplication).sceneActive;
-                    if (s != null && s.objets != null) {
-                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjet)) cibleA = o;
-                    }
-                } else {
-                    java.lang.reflect.Field sceneField = contexteApplication.getClass().getField("sceneActive");
-                    Scene s = (Scene) sceneField.get(contexteApplication);
-                    if (s != null && s.objets != null) {
-                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjet)) cibleA = o;
-                    }
-                }
-            } catch (Exception e) {}
-        }
-        return this.cibleA;
-    }
-    
-    @Override
-    public boolean requiertCibleObjetB() { return true; }
-    
-    @Override
-    public void setCibleObjetB(ObjetBase objet) { 
-        this.cibleB = objet;
-        this.nomCibleObjetB = (objet != null) ? objet.nom : null;
-    }
-    
-    @Override
-    public ObjetBase getCibleObjetB() {
-        if ("__OBJET_IMPLIQUE__".equals(nomCibleObjetB)) return MoteurLogique.dernierObjetImplique;
-
-        if (cibleB == null && nomCibleObjetB != null && contexteApplication != null) {
-            try {
-                if (contexteApplication instanceof InterfaceEditeur) {
-                    Scene s = ((InterfaceEditeur) contexteApplication).sceneActive;
-                    if (s != null && s.objets != null) {
-                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjetB)) cibleB = o;
-                    }
-                } else {
-                    java.lang.reflect.Field sceneField = contexteApplication.getClass().getField("sceneActive");
-                    Scene s = (Scene) sceneField.get(contexteApplication);
-                    if (s != null && s.objets != null) {
-                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjetB)) cibleB = o;
-                    }
-                }
-            } catch (Exception e) {}
-        }
-        return this.cibleB;
-    }
+    public boolean utiliseClavierTexte() { return false; }
 }
 // bas 1
